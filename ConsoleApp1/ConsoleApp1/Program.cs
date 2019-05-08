b15070e3f083dd1be8e04d569a184303eefc95d2
任杰 <49182910+RXJRSJRXM@users.noreply.github.com>
2019-05-08 17:48:31 +0800

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,15 +10,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("bafsdgdfsfbhjasdgfajsdhfgb11111111111111dsfagif");
-=======
+
+            Console.WriteLine("gb11111111111111dsfagif");
             Console.WriteLine("sdadsadsadas");
->>>>>>> 39f1e384802e0dfd7bec809ebc781a01812d47f7
+         
         }
     }
     class  People
     {
-     Console.WriteLine("bafsdgdfsfbhjasdgfajsdhfgb11111111111111dsfagif");
+     Console.WriteLine("bafsdgdfsfbhjasdgff");
     }
 }
