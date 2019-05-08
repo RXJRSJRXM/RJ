8c81588940135e9da09adfe49e5dd418575b86f5
RJ19960822 <1774735073@qq.com>
2019-05-08 17:53:04 +0800

    提交

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,11 +10,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("bafsdgdfsfbhjasdgfajsdhfgb11111111111111dsfagif");
-=======
-            Console.WriteLine("sdadsadsadas");
->>>>>>> 39f1e384802e0dfd7bec809ebc781a01812d47f7
+
+  int[] intarray1 = { 5, 15, 25, 30, 33, 40 };
+            int[] intarray2 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            var query12 =
+                from val1 in intarray1
+                join val2 in intarray2 on val1 % 5 equals val2 % 15
+                select new { VAL1 = val1, VAL2 = val2 };
+            foreach (var val in query12)
+            {
+                Console.WriteLine(val);
+            }
         }
     }
     class  People
