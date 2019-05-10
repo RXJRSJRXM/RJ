4623092f6e6752201997617f5da2cc5bd905ee4f
RJ19960822 <1774735073@qq.com>
2019-05-10 08:57:28 +0800

    二次提交

--- ConsoleApp1/ConsoleApp1/news.cs
+++ ConsoleApp1/ConsoleApp1/news.cs
@@ -10,7 +10,16 @@ namespace ConsoleApp1
     {
         public static void master() 
         {
-        
+        int[] intarray1 = { 5, 15, 25, 30, 33, 40 };
+         int[] intarray2 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            var query12 =
+                from val1 in intarray1
+                join val2 in intarray2 on val1 % 5 equals val2 % 15
+                select new { VAL1 = val1, VAL2 = val2 };
+            foreach (var val in query12)
+            {
+                Console.WriteLine(val);
+            }
         }
     }
 }
