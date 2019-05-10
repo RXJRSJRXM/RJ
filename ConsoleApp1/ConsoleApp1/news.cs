ceae1131b7e04dea76f99510f8648badecc92c5b
任杰 <49182910+RXJRSJRXM@users.noreply.github.com>
2019-05-10 10:31:22 +0800

    Update news.cs

--- ConsoleApp1/ConsoleApp1/news.cs
+++ ConsoleApp1/ConsoleApp1/news.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         public static void master() 
         {
-          Console.WriteLine("叶子楣,林志玲,黄渤,王迅,孙红雷，黄磊，陈惠明,大b哥,午马,张国荣,李寻欢");
+          Console.WriteLine("叶子楣,林志玲,刘德华,武汉,黄石,黄冈,黄渤,王迅,孙红雷，黄磊，陈惠明,大b哥,午马,张国荣,李寻欢");
           int[] intarray1 = { 5, 15, 25, 30, 33, 40 };
           int[] intarray2 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
             var query12 =
@@ -19,7 +19,7 @@ namespace ConsoleApp1
                 select new { VAL1 = val1, VAL2 = val2 };
             foreach (var val in query12)
             {
-                Console.WriteLine(ooo);
+                Console.WriteLine(武汉,黄石,黄冈,黄渤,王迅,孙红雷，黄磊);
             }
 
         }
