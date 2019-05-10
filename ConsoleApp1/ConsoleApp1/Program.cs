a6371a5030df4bfd2b11453ee7c0171ca73dffd3
RJ19960822 <1774735073@qq.com>
2019-05-10 08:33:21 +0800

    提交

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-         Console.WriteLine("蝙蝠中队--刘德华TTTTTTTTTTTT");
+         Console.WriteLine("蝙蝠中队--刘德华,佳林,王力宏");
 
 int[] intarray1 = { 5, 15, 25, 30, 33, 40 };
          int[] intarray2 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
@@ -30,7 +30,7 @@ int[] intarray1 = { 5, 15, 25, 30, 33, 40 };
 
 
 
-   Console.WriteLine("超兽武装--老乌龟LLLLLLLLLLLLLLLLLLL");
+   Console.WriteLine("超兽武装--老乌龟,佳林,王力宏");
 
 }
     }
@@ -41,7 +41,7 @@ int[] intarray1 = { 5, 15, 25, 30, 33, 40 };
 
 
 
-     Console.WriteLine("火灵飞--火系魔法师LLLLLLLLLLLLLLLLLL");
+     Console.WriteLine("火灵飞--火系魔法师,佳林,王力宏");
 
 
 }
