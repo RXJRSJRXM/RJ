d23e442a321f45db5443ac275b13a1346f432545
任杰 <49182910+RXJRSJRXM@users.noreply.github.com>
2019-05-11 10:46:38 +0800

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,10 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
+          int a=32；
+         int b=22；
+         int X=a+b;
+         console.writeLine(X);
 
          Console.WriteLine("蝙蝠中队--刘德华11 11 11 11 11 11");
 
